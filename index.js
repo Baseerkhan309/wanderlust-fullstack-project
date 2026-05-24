33f0d28965a2278d5859c18bcbc5c2b461fe7c7d
Baseer Khan <baseerkhan@gmail.com>
2026-05-24 23:42:22 +0500

    Updated views and modified index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js")
+const Listing = require("./models/listing.js");
+const path = require("path");
 
 // Read from .env
 const MONGO_URL = process.env.MONGO_URL;
@@ -20,16 +21,47 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
- app.get("/listings", (req, res)=>{
-  Listing.find({}).then((res)=>{
-      console.log(res);
-       
-  });
- });
+//Index Route
+ app.get("/listings",async (req, res)=>{
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", {allListings}); 
+});
+
+
+//New Route
+app.get("/listings/new", (req, res)=>{
+    res.render("listings/new.ejs");
+})
+
+//Show Route
+app.get("/listings/:id", async(req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing }); 
+});
+
+//Create Route
+app.post("/listings", async(req, res)=>{
+  let listing = req.body.listing;
+  console.log(listing);
+  
+
+
+  // const newListing = new Listing (req.body.listing);
+  // await newListing.save();
+  // res.redirect("/listings");
+});
+
+
 
 // app.get("/testListing", async (req, res)=>{
 //      let sampleListing = new listing({
