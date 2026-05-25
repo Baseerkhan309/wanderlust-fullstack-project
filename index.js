1f6ca13ef0db8212545a9da6c895e27dda26e389
Baseer Khan <abcd034292618811@gmail.com>
2026-05-25 15:37:10 +0500

    Apply Update and Delete Operation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 // Read from .env
 const MONGO_URL = process.env.MONGO_URL;
@@ -24,7 +25,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
@@ -62,6 +63,21 @@ app.get("/listings/:id/edit",async (req,res)=>{
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
   console.log(listing);
+});
+
+//Update Route
+app.put("/listings/:id", async(req,res)=>{
+        let { id } = req.params;
+      await Listing.findByIdAndUpdate(id, {...req.body.listing});
+      res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async(req, res)=>{
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
 
