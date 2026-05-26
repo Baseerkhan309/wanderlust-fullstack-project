d6ae8728f689e2eebdb4a52d69b170691bd58712
Baseer Khan <abcd034292618811@gmail.com>
2026-05-26 10:32:17 +0500

    Added navbar layout and public assets

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 
 // Read from .env
 const MONGO_URL = process.env.MONGO_URL;
@@ -26,6 +28,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
