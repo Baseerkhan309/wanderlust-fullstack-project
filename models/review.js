4812de039624a4ceb8e3e0bcd4b99846c9585d4b
Baseer Khan <abcd034292618811@gmail.com>
2026-06-10 14:34:53 +0500

    Updated review model and listing views

--- models/review.js
+++ models/review.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewShema = new Schema({
+const reviewSchema = new Schema({
     comment: String,
     rating: {
         type: Number,
