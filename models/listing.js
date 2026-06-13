853152cc1fdf3be6311e97ef0c550427b530bc16
Baseer Khan <abcd034292618811@gmail.com>
2026-06-13 11:00:01 +0500

    Update review and listing page

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
 
 const listingSchema = new Schema({
 
@@ -31,13 +33,19 @@ const listingSchema = new Schema({
     type: String,
     trim: true,
   },
-  reviews:[
+  reviews: [
     {
       type: Schema.Types.ObjectId,
       ref: "Review",
     }
   ]
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } }); 
+  }
+})
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
