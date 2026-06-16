196b6eaa8500024516b7265ccd84a9fde26f465c
Baseer Khan <abcd034292618811@gmail.com>
2026-06-16 23:12:37 +0500

    Add user routes

---
+++ routes/user.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+//  Signup error message if already exist.
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newuser = new User({ email, username });
+        const registeredUser = await User.register(newuser, password);
+        console.log(registeredUser);
+        req.flash("success", "welcome to Wanderlust");
+        res.redirect("/listings");
+
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+
+
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+router.post(
+    "/login",
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true,
+    }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to Wanderlust!");
+        res.redirect("/listings");
+    }
+);
+
+
+module.exports = router;
+
