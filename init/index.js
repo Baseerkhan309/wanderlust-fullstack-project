dce1d1d71456cf340ba7e5516ebeba17f2526896
Baseer Khan <abcd034292618811@gmail.com>
2026-06-18 23:01:39 +0500

    Update initialization script and listing view

--- init/index.js
+++ init/index.js
@@ -5,9 +5,9 @@ const Listing = require("../models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-  .then(async() => {
+  .then(async () => {
     console.log("connected to DB");
-   await initDB();
+    await initDB();
   })
   .catch((err) => {
     console.log(err);
@@ -17,10 +17,14 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const initDB = async()=>{
+const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6a31414deb2846ef5d3085df",
+  }));
   await Listing.insertMany(initData.data);
   console.log("Data Was initialized");
-     
+
 };
 
