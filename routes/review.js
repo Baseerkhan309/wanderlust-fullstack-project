afa3c49a77675f860b747ec3904d027a4ceeb111
Baseer Khan <abcd034292618811@gmail.com>
2026-06-18 23:01:15 +0500

    Update listing and review routes

--- routes/review.js
+++ routes/review.js
@@ -2,27 +2,19 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("../schema.js");
-const reviews = require("../routes/review.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
+const {validateReview, isLoggedIn} = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-}
 
 
 //Post Reviews Route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", validateReview,isLoggedIn, wrapAsync(async (req, res) => {
 
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
+  newReview.author = req.user._id;
+  console.log(newReview);
 
   listing.reviews.push(newReview);
 
