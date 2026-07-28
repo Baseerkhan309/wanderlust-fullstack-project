b12297dff35e10ba911d1d2ae9683ef306f2fc02
Baseer Khan <abcd034292618811@gmail.com>
2026-07-28 23:23:44 +0500

    Update AI controller

--- controllers/AI.js
+++ controllers/AI.js
@@ -6,4 +6,10 @@ const ai = new GoogleGenAI({
 
 module.exports.generateDescription = async (req, res) => {
 
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Say Hello from Gemini.",
+    });
+    const text = response.text;
+    res.send(text);
 };
