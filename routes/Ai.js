c4bd0dc285d933522bdc7dffcb9bfb9b4dfefb91
Baseer Khan <abcd034292618811@gmail.com>
2026-08-01 23:19:56 +0500

    Update AI routes

--- routes/Ai.js
+++ routes/Ai.js
@@ -1,3 +1,7 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); // out own router
 const aiController = require("../controllers/ai");
+
+router.get("/test", aiController.generateDescription);
+
+module.exports = router;
